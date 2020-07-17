acdf541b9ad6688b8083abea4780777991029910
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-07-17 15:52:36 +0300

    temp

---
+++ scripts/second.js
@@ -0,0 +1,43 @@
+// вартість кількість грн
+
+// let money = parseFloat(prompt('Кількість грн',))
+// let price = parseFloat(prompt('Price -',))
+
+// if (money >= price)
+//     document.write('Thanks')
+//  else 
+// document.write('Доплатіть')
+// ===========================
+
+// let userYears = parseInt(prompt('Скільки вам років?', 18))
+// if (userYears >= 18)
+//     {
+//         const answer = prompt('what do you like to drink? \n1) Water \n2) juice \n3) beer',)
+//         if (answer === '3')
+//         document.write('Price 5 $')
+//         else
+//         document.write('For free')
+//     }
+// else
+//     document.write(`Приходь через ${18 - userYears} р`)
+//================================
+
+// let age = parseInt(prompt('Скільки вам років?', 16))
+
+// if (age >= 16 && age < 65) {
+//     document.write('Work!')
+// } else if (age < 16) {
+//     document.write(`Be happy ${16 - age}`)
+// } else {
+//     document.write('Good luck!')
+// }
+
+// ===================================
+// let salary = parseFloat(prompt('Salary'))
+// if (salary < 5000)
+//     salary = 5000
+//     document.write(`Your final salary = ${salary}`)
+// ===================================
+
+
+//  || - обчислює до першого тру --->
