4cf3af4bb8fd350de0cbbec3ef07edd4d3248f3c
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-07-17 16:05:38 +0300

    little fix boolian

--- scripts/second.js
+++ scripts/second.js
@@ -12,11 +12,16 @@
 // let userYears = parseInt(prompt('Скільки вам років?', 18))
 // if (userYears >= 18)
 //     {
-//         const answer = prompt('what do you like to drink? \n1) Water \n2) juice \n3) beer',)
-//         if (answer === '3')
-//         document.write('Price 5 $')
+//         const answer = prompt('Що ти хочеш випити? \n1) Вода \n2) Сік \n3) Пиво',)
+//         if (answer === '1')
+//         document.write('Вода коштує 0 $')
+//         else if (answer === '2')
+//         document.write('Сік коштує 4 $')
+//         else if (answer === '3')
+//         document.write('Пиво коштує 5 $')
 //         else
-//         document.write('For free')
+//         document.write('До побаченя!')
+
 //     }
 // else
 //     document.write(`Приходь через ${18 - userYears} р`)
