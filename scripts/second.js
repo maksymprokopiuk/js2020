3ae1d585035d748a3030687f44faa8af96125e1b
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-07-21 15:48:47 +0300

    the questions about cycles

---
+++ scripts/second.js
@@ -0,0 +1,11 @@
+document.write(`<table border="2px">
+                <tr>`)
+
+for (let i = 0; i < 7; i++) {
+
+    document.write(`<td>${i + 1}</td>`)
+}
+document.write(`
+            </tr>
+            </table>
+`)
