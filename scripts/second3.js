0a1d924aed9585cae90b9f1403f2f43c2bd41919
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-08-05 09:28:32 +0300

    5 task is comleted

--- scripts/second3.js
+++ scripts/second3.js
@@ -15,12 +15,44 @@
 
 // Задача 4. Створити функцію, яка для 3 заданих чисел знаходить кількість парних.
 
+// ==COMPLETED==
 // Задача 5. Створити функції, які переводять:
 // 1) Сантиметри у дюйми;
 // 2) Кілограми у фунти;
 // 3) Кілометри у милі.
+// function santimetersInches(a) {
+//     return a * 0.39370078740157
+// }
+// function kilogramsInPounds(a) {
+//     return a * 2.2046223302272
+// }
+// function kilometersInMiles(a) {
+//     return a * 0.62137119223733
+// }
+// let santimeters = parseFloat(prompt('Сантиметр в дюйм'))
+// document.write(`${santimeters} сантиметрів = ${santimetersInches(santimeters).toFixed(2)} дюймів<br>`)
+// let kilograms = parseFloat(prompt('Кілограми в фунти'))
+// document.write(`${kilograms} кілограм = ${kilogramsInPounds(kilograms).toFixed(2)} фунтів<br>`)
+// let kilometers = parseFloat(prompt('Кілометри в милі'))
+// document.write(`${kilometers} кілометрів = ${kilometersInMiles(kilometers).toFixed(2)} милі`)
 
+// ==COMPLETED==
 // Задача 6. Створити функцію, яка створює таблицю з вказаною кількістю рядків і стовпців
+// let rows = parseInt(prompt('Скільки рядків?', 4))
+// let columns = parseInt(prompt('Скільки стовбців?', 4))
+
+// function table(a, b) {
+//     document.write(`<table border="2px">`)
+//     for (let i = 0; i < rows; i++) {
+//         document.write(`<tr>`)
+//         for (let j = 0; j < columns; j++) {
+//             document.write(`<td>${j + 1}</td>`)
+//         }
+//         document.write(`</tr>`)
+//     }
+//     document.write(`</table>`)
+// }
+// table(rows, columns)
 
 // Задача 7. Створити функцію, яка випадковим чином виводить на екран одне із 4 зображень (шляхи
 // до зображень передаються у функцію)
