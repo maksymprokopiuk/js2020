4bdd40c4155da6ac323449f2e861bee59e85aada
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-08-11 12:53:21 +0300

    1 task

--- scripts/2020_08_10.js
+++ scripts/2020_08_10.js
@@ -4,18 +4,37 @@
 // він відноситься (відмінник, двійочник (має хоча би одну двійку), хорошист (оцінки добре і
 // відмінно), трійочник(є хоча би одна трійка)).
 
-// let points = [2, 3, 4, 5, 5, 4, 3, 2]
+const numberOfEstimates = parseInt(prompt('Ввести кількість оцінок'))
+// random min - max
+function getRandomMinMax(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random()*(max - min +1)) + min
+}
+// створюємо масив
+const points = []
+function createArrRandom() {
+    for (let i = 0; i < numberOfEstimates; i++) {
+        points.push(getRandomMinMax(4, 5))
+    }
+}
+createArrRandom()
+document.write(`Список оцінок - ${points}`)
+document.write('<br>')
 
-// reduce
+// знайти середній бал
+function suma(result, x) {
+    result += x;
+    return result;
+}
 
+let q = points.reduce(suma)
+document.write(`Середнє значення - ${q/numberOfEstimates}<br>`)
 
+// знайти категорію
 
-// (function(){
-//     document.write('cnvbvbv');
-// })()
+if (points.some(mark=>mark===1) || points.some(mark=>mark===2))document.write ('двійочник')
+else if (points.some(mark=>mark===3))document.write ('трійочник')
+else if (points.some(mark=>mark===4))document.write ('хорошист')
+else document.write('відмінник')
 
-// let s = () => document.write('kfkfkfkfk')
-
-// s()
-
-(() => console.log("стрелочная функция"))();
