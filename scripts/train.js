e33a540b9f3368a08dd2cfe5738db920d7501b08
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-08-20 09:23:50 +0300

    learn

--- scripts/train.js
+++ scripts/train.js
@@ -1,3 +1,15 @@
 'use strict'
 
-const myB = '06.08.1983'
+// динамически типизированными
+// восемь основных типов данных в JavaScript
+// 1 number
+// 2 BigInt const bigInt = 1234567890123456789012345678901234567890n;
+// 3 string
+// 4 boolean
+// 5 null - значение
+// 6 undefined - значение
+// 7 object
+// 8 symbol ???
+
+// Оператор typeof
+
