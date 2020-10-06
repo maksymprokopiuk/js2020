9904d3f59f641b993734cee769547c4b8b7f61d2
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-06 13:56:51 +0300

    added lesson 1 banana

---
+++ 2_base_js_advanced/vue_lesson_01_banana/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <my-baner
+    imgSrc="https://rastenievod.com/wp-content/uploads/2018/01/2-13-700x564.jpg"
+    title="Malina"
+    :clicksNumber="2+10"
+    />
+
+    <my-baner
+    imgSrc="https://cosmosmagazine.com/wp-content/uploads/2020/02/190902_banannas_full-1440x813.jpg"
+    title="Banana"
+    :clicksNumber="122"
+    />
+  </div>
+</template>
+
+<script>
+import MyBaner from '@/components/MyBaner' //1. Імпортуємо
+
+export default {
+  name: 'App',
+  components: {
+    MyBaner //2. Реєстрація компонента
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 2_base_js_advanced/vue_lesson_01_banana/src/components/MyBaner/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <img :src="imgSrc" class="my-img">
+        <div>
+            {{`${title} - ${clicksNumber}`}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyBaner',
+
+        props: {
+            imgSrc: {
+                type: String,
+                default: 'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-illustration-132483053.jpg'
+            },
+            title: {
+                type: String,
+                default: 'No title'
+            },
+            clicksNumber: {
+                type: Number,
+                default: 0
+            }
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+    .my-img {
+        width: 200px;
+    }
+</style>
