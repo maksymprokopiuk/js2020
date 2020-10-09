e50e88116cecf242a27710aaefea227a71b4b661
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-09 13:01:56 +0300

    added hw 'v-for' for list

---
+++ 2_base_js_advanced/vue_lesson_04_hw_v-for_in/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <list
+    :myList="myList"
+    />
+  </div>
+</template>
+
+<script>
+import List from '@/components/List'
+
+export default {
+  name: 'App',
+  components: {
+    List
+  },
+  data() {
+    return {
+      myList: [
+        {
+          name: 'Core Docs 2',
+          link: 'https://vuejs.org'
+        },
+        {
+          name: 'Forum 2',
+          link: 'https://forum.vuejs.org'
+        },
+        {
+          name: 'Community Chat 2',
+          link: 'https://chat.vuejs.org'
+        },
+        {
+          name: 'Twitter 2',
+          link: 'https://twitter.com/vuejs'
+        },
+        {
+          name: 'News 2',
+          link: 'https://news.vuejs.org'
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ 2_base_js_advanced/vue_lesson_04_hw_v-for_in/src/components/List/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <ul>
+            <li><a href="https://vuejs.org">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org">Forum</a></li>
+            <li><a href="https://chat.vuejs.org">Community Chat</a></li>
+            <li><a href="https://twitter.com/vuejs">Twitter</a></li>
+            <li><a href="https://news.vuejs.org">News</a></li>
+        </ul>
+        <ul>
+            <li v-for="item in myList" :key="item.name"><a v-bind:href="item.link" class="green"> {{item.name}} </a></li>
+        </ul>
+        <hr>
+        <select>
+            <option>Core Docs</option>
+            <option>Forum</option>
+            <option>Community Chat</option>
+            <option>Twitter</option>
+            <option>News</option>
+        </select>
+        <select>
+            <option v-for="el in myList" :key="el.name">{{el.name}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'List',
+
+        props: {
+            myList: {
+                type: Array,
+                default: ()=>[]
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    a {
+        text-decoration: none;
+    }
+    li {
+        list-style: none;
+        font-size: 1.5rem;
+    }
+    .green {
+        color: green;
+    }
+</style>
