4f91899fc3a923792d753ef1b32c90439a8097cd
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-13 09:14:44 +0300

    added lesson 5 (store product)

--- 2_base_js_advanced/05_lesson/src/App.vue
+++ 2_base_js_advanced/05_lesson/src/App.vue
@@ -1,18 +1,165 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-list :products="myProducts"/>
+    <todo-list />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "./components/TodoList";
+import ProductList from "./components/ProductList";
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoList,
+    ProductList
+  },
+
+  data() {
+    return {
+      myProducts:[
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:1,
+          title:'Tel',
+          price:1200,
+          img:'https://i2.rozetka.ua/goods/19989681/samsung_galaxy_m51_6_128gb_black_images_19989681366.jpg',
+          category:'phone',
+          year:2010
+        },
+        {
+          id:2,
+          title:'Tel2',
+          price:12040,
+          img:'https://i2.rozetka.ua/goods/17831432/apple_iphone_se_64gb_white_images_17831432779.jpg',
+          category:'phone',
+          year:2010
+        },
+      ],
+
+      myList: [
+        {
+          id:1,
+          task:"Milk",
+          time:'18.10',
+          status:"NEW"
+        },
+        {
+          id:2,
+          task:"Film",
+          time:'19.10',
+          status:"IN-PROGRES"
+        },
+        {
+          id:3,
+          task:"Slip",
+          time:'19.50',
+          status:"POSTPONED"
+        },
+        {
+          id:4,
+          task:"Milk2",
+          time:'18.10',
+          status:"NEW"
+        },
+      ]
+    }
+  },
 }
 </script>
 
@@ -21,7 +168,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
