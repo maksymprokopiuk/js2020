6ecbaf8581063deb4ccbdb01c3edf7685d6dffe9
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-14 21:00:12 +0300

    addede 06 lesson 'Store'

---
+++ 2_base_js_advanced/06_lesson/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <store/>
+    <!-- <todo-list /> -->
+  </div>
+</template>
+
+<script>
+// import TodoList from "./components/TodoList";
+import Store from "./components/Store";
+export default {
+  name: 'App',
+  components: {
+    // TodoList,
+    Store
+  },
+
+  data() {
+    return {
+     
+
+      myList: [
+        {
+          id:1,
+          task:"Milk",
+          time:'18.10',
+          status:"NEW"
+        },
+        {
+          id:2,
+          task:"Film",
+          time:'19.10',
+          status:"IN-PROGRES"
+        },
+        {
+          id:3,
+          task:"Slip",
+          time:'19.50',
+          status:"POSTPONED"
+        },
+        {
+          id:4,
+          task:"Milk2",
+          time:'18.10',
+          status:"NEW"
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
