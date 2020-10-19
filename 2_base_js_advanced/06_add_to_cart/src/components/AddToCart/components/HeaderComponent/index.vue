c2fe980c52dd2846fd8af7f0b79ec5d9938bfc4f
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-19 17:30:48 +0300

    styles

--- 2_base_js_advanced/06_add_to_cart/src/components/AddToCart/components/HeaderComponent/index.vue
+++ 2_base_js_advanced/06_add_to_cart/src/components/AddToCart/components/HeaderComponent/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="header-component">
         <ul v-if="items[0]">
             <li v-for="(item) in items" :key="item.id">
                 {{item.id}} {{item.name}} {{item.surname}}
@@ -28,7 +28,17 @@
 </script>
 
 <style scoped>
+.header-component {
+    background-color: antiquewhite;
+    width: 100%;
+    height: 100px;
+}
     ul {
-        border: 1px solid #000;
+        /* border: 1px solid #000; */
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        list-style: none;
     }
 </style>
