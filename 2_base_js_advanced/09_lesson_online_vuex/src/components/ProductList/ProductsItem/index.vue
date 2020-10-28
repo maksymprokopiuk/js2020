b1f89ce963226fe05b16f26c4bf0f1010b11789b
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-28 09:51:28 +0200

    created ProductsItem, ProductList

---
+++ 2_base_js_advanced/09_lesson_online_vuex/src/components/ProductList/ProductsItem/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <img :src="image" :alt="title">
+        <div>{{title}}</div>
+        <div>{{price}}</div>
+        <button @click="onAdd">Add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProductsItem',
+
+        props: {
+            id: {
+                type: [String, Number],
+                require
+            },
+            image: {
+                type: String,
+                default: require('@/assets/no_image.png')
+            },
+            title: {
+                type: String,
+                default: 'no title'
+            },
+            price: {
+                type: [String, Number],
+                default: 'no price'
+            }
+        },
+
+        methods: {
+            onAdd() {
+                
+            }
+        },
+    }
+</script>
+
+<style scoped>
+.container {
+    width: 200px;
+    height: 300px;
+    border-radius: 5px;
+}
+</style>
---
+++ 2_base_js_advanced/09_lesson_online_vuex/src/components/ProductList/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProductList'
+    }
+</script>
+
+<style scoped>
+
+</style>
