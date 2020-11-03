490716e852d98aa45664afd0fdfb1e63be4a7141
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-11-03 14:19:08 +0200

    created list

--- 2_base_js_advanced/11_hw_cards/src/App.vue
+++ 2_base_js_advanced/11_hw_cards/src/App.vue
@@ -10,7 +10,50 @@ export default {
   name: 'App',
   components: {
     // HelloWorld
-  }
+  },
+
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          word: 'First',
+          cardsDataList: [
+            {src: '@/assets/img/1.png', title: '1.1'},
+            {src: '@/assets/img/2.png', title: '1.2'},
+            {src: '@/assets/img/3.png', title: '1.3'},
+            {src: '@/assets/img/4.png', title: '1.4'},
+            {src: '@/assets/img/5.png', title: '1.5'},
+            {src: '@/assets/img/6.png', title: '1.6'},
+          ]
+        },
+        {
+          id: 2,
+          word: 'Second',
+          cardsDataList: [
+            {src: '@/assets/img/1.png', title: '2.1'},
+            {src: '@/assets/img/2.png', title: '2.2'},
+            {src: '@/assets/img/3.png', title: '2.3'},
+            {src: '@/assets/img/4.png', title: '2.4'},
+            {src: '@/assets/img/5.png', title: '2.5'},
+            {src: '@/assets/img/6.png', title: '2.6'},
+          ]
+        },
+        {
+          id: 3,
+          word: 'Third',
+          cardsDataList: [
+            {src: '@/assets/img/1.png', title: '3.1'},
+            {src: '@/assets/img/2.png', title: '3.2'},
+            {src: '@/assets/img/3.png', title: '3.3'},
+            {src: '@/assets/img/4.png', title: '3.4'},
+            {src: '@/assets/img/5.png', title: '3.5'},
+            {src: '@/assets/img/6.png', title: '3.6'},
+          ]
+        },
+      ]
+    }
+  },
 }
 </script>
 
