4e0c9c1d402c4ec371d986c922a8d05ee34fad66
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-11-06 12:34:17 +0200

    create new project 'Routes'

---
+++ 2_base_js_advanced/00_routes/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-routes/>
+  </div>
+</template>
+
+<script>
+import AppRoutes from './components/AppRoutes'
+
+export default {
+  name: 'App',
+  components: {
+    AppRoutes
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 2_base_js_advanced/00_routes/src/components/AppRoutes/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        AppRoutes
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppRoutes'
+    }
+</script>
+
+<style scoped>
+
+</style>
