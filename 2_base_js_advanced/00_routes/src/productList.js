83e2661d9130df9c5523c3dbc1cb7e12d019468a
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-11-07 11:29:40 +0200

    created 'productList.js'

---
+++ 2_base_js_advanced/00_routes/src/productList.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        title: 'Banana',
+        img: 'https://cdn.the-scientist.com/assets/articleNo/42182/aImg/35036/bananas-thumb-l.png',
+    },
+    {
+        id: 2,
+        title: 'Pineapple',
+        img: 'https://media.self.com/photos/5b4371cc4d0c3c282a8878d3/4:3/w_2560%2Cc_limit/pineapple.jpg',
+    },
+    {
+        id: 3,
+        title: 'Kivi',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0ldxcNoF34BRrO8zaBM7S4mcjRIXV3VF0gw&usqp=CAU',
+    },
+]
