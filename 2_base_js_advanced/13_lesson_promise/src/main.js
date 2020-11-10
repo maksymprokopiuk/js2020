8e2c19be50b97463a6da0da1654a6c386361d95e
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-11-10 11:04:40 +0200

    added Buefy

--- 2_base_js_advanced/13_lesson_promise/src/main.js
+++ 2_base_js_advanced/13_lesson_promise/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 import store from "./store";
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 new Vue({
