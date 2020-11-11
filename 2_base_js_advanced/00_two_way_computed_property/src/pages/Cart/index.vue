12aca77772757d89a535c300fb19454f208844f2
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-11-11 08:31:53 +0200

    created two-way computed property

--- 2_base_js_advanced/00_two_way_computed_property/src/pages/Cart/index.vue
+++ 2_base_js_advanced/00_two_way_computed_property/src/pages/Cart/index.vue
@@ -2,19 +2,31 @@
     <div class="cart">
         <label>
             <p>Cart</p>
-            <input type="text" disabled :value="getTestNum">
+            <input type="text" v-model="testNum">
         </label>
     </div>
 </template>
 
 <script>
-    import { mapGetters } from "vuex";
+    import { mapGetters, mapActions } from "vuex";
 
     export default {
         name: 'Cart',
 
         computed: {
             ...mapGetters(['getTestNum']),
+            testNum: {
+                get: function() {
+                    return this.getTestNum
+                },
+                set: function(val) {
+                    return this.setTestNum(val)
+                }
+            }
+        },
+
+        methods: {
+            ...mapActions(['setTestNum'])
         },
     }
 </script>
