cbee0dbd105a6d9a2e39019c6225ab2bef086b89
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-12-15 13:36:02 +0200

    added cinamas and movies pages

--- 2_base_js_advanced/23_hw_cinema/cinema/app.js
+++ 2_base_js_advanced/23_hw_cinema/cinema/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
+var cinemasRouter = require('./routes/cinemas');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
+app.use('/cinemas', cinemasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
