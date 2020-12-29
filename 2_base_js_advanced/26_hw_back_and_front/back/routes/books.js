03c0ded03813c87f10ac2978fe531c485210e833
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-12-29 19:34:34 +0200

    26 hw created back with rouer delete book

--- 2_base_js_advanced/26_hw_back_and_front/back/routes/books.js
+++ 2_base_js_advanced/26_hw_back_and_front/back/routes/books.js
@@ -8,6 +8,7 @@ const Schema = mongoose.Schema;
 
 const bookScheme = new Schema({
     title: String,
+    picture: String,
     price: Number
 });
 
@@ -17,18 +18,29 @@ router.get('/', function(req, res, next) {
   res.send('BOOKS');
 });
 
-// localhost:3000/books/add
+// ADD BOOK localhost:3000/books/add
 router.get('/add', function (req, res, next) {
   const book = new Book({
     title: req.query.title,
+    picture: req.query.picture,
     price: req.query.price
   });
 
   book.save(function (err, prod) {
-    if (err) return res.status(500).json({ err: { msg: "Saving faild!" } })
+    if (err) return res.status(500).json({ err: { msg: "Saving failed!" } })
     res.status(200).json({ msg: "Saving is good!" })
     mongoose.disconnect();
   });
 });
+
+// DELETE BOOK localhost:3000/books/delete/5feb67b17164e130a80ae754
+router.get('/delete/:id', function (req, res, next){
+  console.log(req.params.id);
+  Book.findOneAndDelete({_id: req.params.id}, function (err, doc) {
+    if(err) return res.status(500).json({err:{msg: 'Deleting failed!'}})
+    res.status(200).json({ msg: "Deleting is good!" })
+    mongoose.disconnect();
+  })
+})
 
 module.exports = router;
