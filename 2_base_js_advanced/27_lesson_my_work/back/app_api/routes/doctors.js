89df9daad78c2a02616a35eede4e1544c7a728c7
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2021-01-03 12:24:37 +0200

    27 my w - doctors f work

--- 2_base_js_advanced/27_lesson_my_work/back/app_api/routes/doctors.js
+++ 2_base_js_advanced/27_lesson_my_work/back/app_api/routes/doctors.js
@@ -1,13 +1,13 @@
-const express=require('express')
-const router=express.Router()
-const ctrDoctors=require('../controllers/doctors')
+const express = require('express')
+const router = express.Router()
+const ctrDoctors = require('../controllers/doctors')
 const bodyParser = require("body-parser");
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/' , ctrDoctors.getDoctors);
+router.get('/', ctrDoctors.getDoctors); // get all doctors or filter
 router.get('/:doctorid', ctrDoctors.getDoctorById);
 router.post('/', urlencodedParser, ctrDoctors.addDoctor);
 router.put('/', urlencodedParser, ctrDoctors.updateDoctor);
-router.delete('/:doctorid',ctrDoctors.deleteDoctor);
+router.delete('/:doctorid', ctrDoctors.deleteDoctor);
 
-module.exports=router
+module.exports = router
